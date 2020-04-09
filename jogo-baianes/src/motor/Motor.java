c32f569ab1718209dab834617f6c12ec1469782d
Lucas V. Lopez <lucasviniciuslopez@gmail.com>
2020-04-09 17:45:18 -0300

    New Motor, Partida and Pergunta methods

--- jogo-baianes/src/motor/Motor.java
+++ jogo-baianes/src/motor/Motor.java
@@ -8,6 +8,7 @@ import utilidades.ImportadorDeJson;
 
 public class Motor {
 	private ArrayList<Pergunta> todasAsPerguntas = ImportadorDeJson.jsonParaArray(new File(".//src//files//dicionario.json"));
+	private ArrayList<Partida> ultimasDuasPartidas = new ArrayList<Partida>();
 	private Random r = new Random();
 	private final int limite = todasAsPerguntas.size();
 	
@@ -16,31 +17,55 @@ public class Motor {
 		
 	}
 		
-	public Partida criarPartida (int numeroDePerguntas) {
-		Partida partida = new Partida();
-		partida.setInicioPartida(System.currentTimeMillis()/1000);
+	public Partida criarPartida (int numeroDePerguntas, String nomeDoJogador) {
+		/**
+		 * Partida sai daqui já iniciada.
+		 */
 		ArrayList<Pergunta> perguntasSelecionadas = new ArrayList<Pergunta>();
 		for (int i = 0 ; i < numeroDePerguntas ; i++) {
-			perguntasSelecionadas.add(todasAsPerguntas.get(r.nextInt(limite + 1)));
+			perguntasSelecionadas.add(todasAsPerguntas.get(r.nextInt(limite)));
 			if (i > 0) { //testa se houve pergunta repetida
 				if (perguntasSelecionadas.get(i-1).equals(perguntasSelecionadas.get(i))) {
 					perguntasSelecionadas.remove(i);
-					perguntasSelecionadas.add(todasAsPerguntas.get(r.nextInt(limite + 1)));
+					perguntasSelecionadas.add(todasAsPerguntas.get(r.nextInt(limite)));
 				}
 			}
 		}
-		partida.setPerguntas();
-		return partida;
-		
+		Partida p = new Partida(System.currentTimeMillis()/1000, perguntasSelecionadas);
+		p.setJogador(nomeDoJogador);
+		return p;
 	}
+	
+	/*
+	public void iniciarPartida (Partida p) {
+		p.setInicioPartida(System.currentTimeMillis()/1000);
+		//esse método acima é privado.
+		//O método INICIARPARTIDA é um apêndice.
+	}
+	*/
 		
 	public void finalizarPartida (Partida p) {
-		
-			
+		p.setFimPartida(System.currentTimeMillis()/1000);
+		if (ultimasDuasPartidas.size() == 2) ultimasDuasPartidas.remove(0); //se o tamanho for 2, remova o primeiro
+		//o de cima poderia ser uma pilha, né? Eu estive pensando nisso...
+		ultimasDuasPartidas.add(p);
 	}
 		
 	public String quemGanhou () {
-			
+		String resposta = "NADA";
+		if (ultimasDuasPartidas.get(0).getPontos() > ultimasDuasPartidas.get(1).getPontos())
+			return ultimasDuasPartidas.get(0).getJogador();
+		if (ultimasDuasPartidas.get(1).getPontos() > ultimasDuasPartidas.get(0).getPontos())
+			return ultimasDuasPartidas.get(1).getJogador();
+		if (ultimasDuasPartidas.get(1).getPontos() == ultimasDuasPartidas.get(0).getPontos()) {
+			if (ultimasDuasPartidas.get(0).getTempo() > ultimasDuasPartidas.get(1).getTempo())
+				return ultimasDuasPartidas.get(0).getJogador();
+			if (ultimasDuasPartidas.get(1).getTempo() > ultimasDuasPartidas.get(0).getTempo())
+				return ultimasDuasPartidas.get(1).getJogador();
+		}
+		
+		return resposta;
+
 	}
 
 }
--- jogo-baianes/src/motor/Partida.java
+++ jogo-baianes/src/motor/Partida.java
@@ -1,15 +1,70 @@
 package motor;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Partida {
 	private long duracao;
 	private String jogador;
 	private int pontos;
+	private ArrayList<Pergunta> perguntasDaPartida;
+	private int contador = 0;
+	private boolean partidaFinalizada = false;
 	
-	public Partida() {
-		
+	public Partida(long tempo, ArrayList<Pergunta> perguntas) {
+		setInicioPartida(tempo);
+		setPerguntas(perguntas);
 	}
 	
-	public void setInicioPartida (long tempo) {
+	public void setJogador (String s) {
+		this.jogador = s;
+	}
+	
+	public String getJogador () {
+		return jogador;
+	}
+	
+	private void setInicioPartida (long tempo) {
 		this.duracao = tempo;
+	}
+	
+	private void setPerguntas (ArrayList<Pergunta> ps) {
+		perguntasDaPartida = ps;
+	}
+	
+	public void getNextPergunta () {
+		/**
+		 * Retorna a pergunta como um System.out.println(); diretamente no console mais próximo.
+		 */
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(perguntasDaPartida.get(contador).getPergunta());
+		System.out.println(" ");
+		System.out.println("A) " + perguntasDaPartida.get(contador).getRespostas().get(0));
+		System.out.println("B) " + perguntasDaPartida.get(contador).getRespostas().get(1));
+		System.out.println("C) " + perguntasDaPartida.get(contador).getRespostas().get(2));
+		System.out.println("Digite sua resposta:");
+		char respostaUsuario = Character.toLowerCase(scanner.next().charAt(0));
+		if (perguntasDaPartida.get(contador).getCorreta() == respostaUsuario) pontos++;
+		scanner.close();
+	}
+	
+	public boolean isLastPergunta() {
+		/**
+		 * Retorna se é a última pergunta.
+		 * Bom para usar como condicional de um loop para passar as perguntas.
+		 */
+		return (perguntasDaPartida.size() == contador);
+	}
+	
+	public void setFimPartida (long tempoFinal) {
+		duracao = tempoFinal - duracao;
+	}
+	
+	public int getPontos() {
+		return pontos;
+	}
+	
+	public long getTempo() {
+		return duracao;
 	}
 }
